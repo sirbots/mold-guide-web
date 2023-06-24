f58c203f9de902ab42e8bf0a9f374341c314fece
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-06-24 10:48:26 -0700

    Started About page.

--- app/page.js
+++ app/page.js
@@ -28,34 +28,33 @@ export default function Home() {
       />
 
       {/* Page Content */}
-      <div className={styles.homeContent}>
+      <div className={styles.pageContent}>
         <h2 className={lora.className}>Do You Have a Mold Problem?</h2>
 
-        <div className={styles.intro}>
-          <p className={styles.text}>
-            If you're here, then there's a good chance that you're not feeling
-            great.
-          </p>
-          <p>
-            You might feel sick or you might feel like your body and mind are
-            falling apart. Mold illness can manifest in all sorts of strange and
-            debilitating ways.
-          </p>
-          <p>
-            Maybe you're plagued with anxiety and depression, chronic fatigue,
-            or constant muscle and joint aches. Maybe you're coughing more or
-            having trouble sleeping.
-          </p>
-          <p>
-            Maybe you're suffering from brain fog, cognitive impairment, or
-            memory loss. You're reacting to foods that never bothered you before
-            or you developed eczema, psorisasis, or weird rashes.
-          </p>
-          <p>
-            And worst of all, mold illness seems to just drain the joy out of
-            life.
-          </p>
-        </div>
+        <p className={styles.text}>
+          If you're here, then there's a good chance that you're not feeling
+          great.
+        </p>
+        <p>
+          You might feel sick or you might feel like your body and mind are
+          falling apart. Mold illness can manifest in all sorts of strange and
+          debilitating ways.
+        </p>
+        <p>
+          Maybe you're plagued with anxiety and depression, chronic fatigue, or
+          constant muscle and joint aches. Maybe you're coughing more or having
+          trouble sleeping.
+        </p>
+        <p>
+          Maybe you're suffering from brain fog, cognitive impairment, or memory
+          loss. You're reacting to foods that never bothered you before or you
+          developed eczema, psorisasis, or weird rashes.
+        </p>
+        <p>
+          And worst of all, mold illness seems to just drain the joy out of
+          life.
+        </p>
+
         <div className={styles.steps}>
           <h2 style={lora.style}>Dealing with Mold, One Step at a Time</h2>
 
