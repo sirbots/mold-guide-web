84d7333e4345878dbfcca92975108a38a88b7736
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-06-29 14:52:54 -0700

    Trying a different tutorial

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,3 +1,5 @@
+import { prisma } from "../lib/prisma";
+import { compare } from "bcryptjs";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -17,8 +19,26 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const user = { id: "1", name: "Admin", email: "admin@admin.com" };
-        return user;
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !(await compare(credentials.password, user.password))) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          randomKey: "Hey cool",
+        };
       },
     }),
   ],
