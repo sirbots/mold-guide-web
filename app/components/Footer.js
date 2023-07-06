a56e247548ab13933f13da0820129a3561953628
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-07-06 16:19:32 -0700

    Add disclaimer to footer.

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,6 +3,10 @@ import styles from "../page.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <p>
+        This site is for informational purposes only. Nothing on this site
+        should be construed as medical advice.
+      </p>
       <p>&copy; {new Date().getFullYear()} TheMoldGuide.com</p>
     </footer>
   );
