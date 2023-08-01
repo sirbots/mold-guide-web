36e033de1b65e130c2d4e4314bb8292ffedbffe9
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-08-01 16:16:09 -0700

    Add logo.

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,7 @@
+// Styes & Images
 import styles from "../page.module.css";
+import Image from "next/image";
+import logo from "../../public/logos/logo_with_text.png";
 
 // Auth
 import { authOptions } from "../lib/auth";
@@ -22,17 +25,27 @@ export default async function Header() {
   return (
     <div className={styles.header}>
       {/* Desktop Nav Links */}
-      <ul className={styles.desktopNav}>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/practitioners">Find Mold Doctors</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        {/* {session ? (
+      <div className={styles.logoBox}>
+        <a href="/">
+          <Image
+            src={logo}
+            className={styles.logoImg}
+            alt="The Mold Guide logo"
+          />
+        </a>
+      </div>
+      <div className={styles.navLinks}>
+        <ul className={styles.desktopNav}>
+          {/* <li>
+            <a href="/">Home</a>
+          </li> */}
+          <li>
+            <a href="/practitioners">Find Mold Doctors</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          {/* {session ? (
           <li>
             <a href="/profile">Profile</a>
           </li>
@@ -46,20 +59,21 @@ export default async function Header() {
             </li>
           </>
         )} */}
-      </ul>
+        </ul>
 
-      {/* Mobile Nav Links */}
-      <ul className={styles.mobileNav}>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/practitioners">Find Mold Doctors</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-      </ul>
+        {/* Mobile Nav Links */}
+        <ul className={styles.mobileNav}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/practitioners">Find Mold Doctors</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
