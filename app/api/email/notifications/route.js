8dc298eb5c0a97aaa466739a694044a2858fcf94
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-08-30 20:28:06 -0700

    Change notification recipient to live email address

--- app/api/email/notifications/route.js
+++ app/api/email/notifications/route.js
@@ -8,12 +8,10 @@ export async function POST(request) {
   const json = await request.json();
 
   try {
-    //  const formMessage = request.formSubmitted;
-
     const data = await resend.emails.send({
       from: "The Mold Guide <notifications@themoldguide.com>",
       // You can send to delivered@resend.dev to test delivery w/o compromsing your domain reputation
-      to: ["delivered@resend.dev"],
+      to: ["hello@themoldguide.com"],
       subject: "Mold Guide: New " + json.formSubmitted + " Submitted",
       react: EmailFormNotificationTemplate({
         // firstName: "Robert",
