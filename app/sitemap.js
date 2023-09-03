510dcae254cdda7bb08144bd9aea5303096e319f
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-09-02 21:17:43 -0700

    Add resources page and articles pages to sitemap generator.

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,9 +1,10 @@
 import { prisma } from "./lib/prisma";
+import { getArticles } from "./lib/contentful";
 
 // Define the base URL
 const URL = "https://themoldguide.com/";
 
-// Get all of the practitioners
+// Get all of the practitioners from MongoDB
 const practitionersArray = await prisma.doctor.findMany({
   where: {
     published: {
@@ -12,14 +13,26 @@ const practitionersArray = await prisma.doctor.findMany({
   },
 });
 
-// Create an empty array for the doctor data
+// Get all the articles from Contentful
+const articlesArray = await getArticles();
+
+// Create empty arrays for the doctor and article data
 const doctorData = [];
+const articleData = [];
 
 // Iterate through the practitionersArray and push an object with the slug and lastModified data for each practitioner
 practitionersArray.forEach((practitioner) =>
   doctorData.push({
     url: URL + "practitioners/" + practitioner.slug,
     lastModified: practitioner.lastModified,
+  })
+);
+
+// Iterate through the articlesArray and push an object with the slug and lastModified data for each practitioner
+articlesArray.forEach((article) =>
+  articleData.push({
+    url: URL + "resources/articles/" + article.fields.slug,
+    lastModified: article.sys.updatedAt,
   })
 );
 
@@ -50,10 +63,15 @@ const staticUrls = [
     url: URL + "signup",
     lastModified: new Date(),
   },
+  {
+    // Resources Page
+    url: URL + "resources",
+    lastModified: new Date(),
+  },
   // TO DO: insert other status pages here as you create them
 ];
 
-const combinedUrls = staticUrls.concat(doctorData);
+const combinedUrls = staticUrls.concat(doctorData, articleData);
 
 export default async function sitemap() {
   // Return the combinedUrls array of objects to Nextjs sitemap() function
