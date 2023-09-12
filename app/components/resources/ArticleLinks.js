ffea0e2e772ae733aa9837012383ab4749bb3852
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-09-12 10:51:28 -0700

    Display articles by category.

--- app/components/resources/ArticleLinks.js
+++ app/components/resources/ArticleLinks.js
@@ -10,7 +10,80 @@ export default async function ArticleLinks({}) {
   return (
     <div>
       <h2>Articles</h2>
-      {/* Display all of the currently published articles with their title and a link to the single article page. */}
+
+      {/* Display the currently published articles by category, with their title and a link to the single article page. */}
+      <h3>Mold Prevention</h3>
+      {articles &&
+        articles
+          .filter((article) => article.fields.category.includes("Prevention"))
+
+          .map((article) => (
+            <div key={article.sys.id}>
+              <p>
+                <a href={"/resources/articles/" + article.fields.slug}>
+                  {article.fields.articleTitle}
+                </a>
+              </p>
+            </div>
+          ))}
+      <h3>Mold Testing</h3>
+      {articles &&
+        articles
+          .filter((article) => article.fields.category.includes("Testing"))
+
+          .map((article) => (
+            <div key={article.sys.id}>
+              <p>
+                <a href={"/resources/articles/" + article.fields.slug}>
+                  {article.fields.articleTitle}
+                </a>
+              </p>
+            </div>
+          ))}
+      {/* <h3>Mold Remediation</h3>
+      {articles &&
+        articles
+          .filter((article) => article.fields.category.includes("Remediation"))
+
+          .map((article) => (
+            <div key={article.sys.id}>
+              <p>
+                <a href={"/resources/articles/" + article.fields.slug}>
+                  {article.fields.articleTitle}
+                </a>
+              </p>
+            </div>
+          ))} */}
+      <h3>Treatment</h3>
+      {articles &&
+        articles
+          .filter((article) => article.fields.category.includes("Treatment"))
+
+          .map((article) => (
+            <div key={article.sys.id}>
+              <p>
+                <a href={"/resources/articles/" + article.fields.slug}>
+                  {article.fields.articleTitle}
+                </a>
+              </p>
+            </div>
+          ))}
+      {/* <h3>Cleaning</h3>
+      {articles &&
+        articles
+          .filter((article) => article.fields.category.includes("Cleaning"))
+
+          .map((article) => (
+            <div key={article.sys.id}>
+              <p>
+                <a href={"/resources/articles/" + article.fields.slug}>
+                  {article.fields.articleTitle}
+                </a>
+              </p>
+            </div>
+          ))} */}
+
+      {/* <h3>All Articles</h3>
       {articles &&
         articles.map((article) => (
           <div key={article.sys.id}>
@@ -20,7 +93,7 @@ export default async function ArticleLinks({}) {
               </a>
             </p>
           </div>
-        ))}
+        ))} */}
     </div>
   );
 }
