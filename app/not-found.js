98f0a6bb0af1db4855202ad0861ec4c60fae1fc9
Robert Carter <24420780+sirbots@users.noreply.github.com>
2023-09-18 12:26:10 -0400

    Cleanup

--- app/not-found.js
+++ app/not-found.js
@@ -1,5 +1,4 @@
 import Header from "./components/layout/Header";
-import Hero from "./components/layout/Hero";
 import WideImage from "./components/layout/WideImage";
 import Footer from "./components/layout/Footer";
 
